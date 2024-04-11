daf65a60716611ab6b6300f6711fd418425f99fd
ВАШÐAnastasia <anastasija2003@mail.ru>
2024-04-11 18:09:14 +0300

    DZ5

---
+++ app/src/main/java/com/example/apphomework05/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.apphomework05
+
+import android.os.Bundle
+import androidx.core.app.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
